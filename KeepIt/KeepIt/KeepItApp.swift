221c1f12daf7ef916d5be049b20b9032bd404f36
Ruban <65346010+sk-ruban@users.noreply.github.com>
2025-08-06 13:45:47 +0800

    feat: initialise visionOS project

---
+++ KeepIt/KeepIt/KeepItApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepItApp.swift
+//  KeepIt
+//
+//  Created by Ruban on 2025-08-06.
+//
+
+import SwiftUI
+
+@main
+struct KeepItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
